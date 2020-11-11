82c3338ee6b836099ce71d97908bc7d5faf1f0fd
Caroline Manson <carolinemanson15@gmail.com>
2020-11-11 18:24:28 -0500

    added dependencies, changed index.js

--- index.js
+++ index.js
@@ -1,56 +1,85 @@
-// node js basics per introduction to nodejs class
-
-console.log('This is my cool app')
-console.log(process.argv)
-
-//must put "add" "1" and "2" into the array by putting them after node index in the CLI
-
-let num1 = process.argv[3]
-let num2 = process.argv[4]
-let operator = process.argv[2]
-
-// must parse the numbers (which are strings at this point) to make them integers
-if (operator == 'add') {
-    console.log(parseInt(num1) + parseInt(num2))
-} else {
-    console.log('Please read user docs')
-}
-
-// inquirer skeleton code for asking questions in the command line (per documentation)
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  })
-
   // fs using writeFile method -- use this to create your .md file for the README
-
   const fs = require('fs')
   let item = process.argv[2]
 
-  fs.writeFile('README.md', item, function(error){
-    if (error) {
-        console.log(error)
-    }
-    console.log('Way to go!')
-  })
-
-  // fs using appendFile method to add to existing .md file
-
-  fs.appendFile('README.md', process.argv[2] + '\n', function(error) {
+  fs.writeFile('README.md', item, function (error) {
       if (error) {
           console.log(error)
-      } else {
-          console.log('Commit logged!')
       }
+    //   console.log('Way to go!')
   })
+
+  // inquirer skeleton code for asking questions in the command line (per documentation)
+  var inquirer = require('inquirer');
+  inquirer
+      .prompt([
+          /* Pass your questions in here */
+          {
+              type: "input",
+              message: "What is the title of your project?",
+              name: "title"
+          },
+          {
+              type: "input",
+              message: "What is the description of your project?",
+              name: "description"
+          },
+          {
+              type: "input",
+              message: "How do you install your project?",
+              name: "installation"
+          },
+          {
+              type: "input",
+              message: "How do you use your project?",
+              name: "usage"
+          },
+          {
+              type: "input",
+              message: "What is your project's license?",
+              name: "license"
+          },
+          {
+              type: "input",
+              message: "Does your project have any guidelines for contributions?",
+              name: "contributing"
+          },
+          {
+              type: "input",
+              message: "Do you have any tests written for this project?",
+              name: "tests"
+          },
+          {
+              type: "input",
+              message: "What is your GitHub username?",
+              name: "username"
+          },
+          {
+              type: "input",
+              message: "What is your email address?",
+              name: "email"
+          },
+      ])
+      .then(answers => {
+          // Use user feedback for... whatever!!
+          // .then(function(renderMd))
+          
+    // fs using appendFile method to add to existing .md file
+
+    fs.appendFile('README.md', process.argv[2] + '\n', function(error) {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log('Commit logged!')
+        }
+    })
+      })
+      .catch(error => {
+          if (error.isTtyError) {
+              // Prompt couldn't be rendered in the current environment
+              console.log(error)
+          } else {
+              // Something else when wrong
+              console.log('Oh no! Something else is wrong')
+          }
+      })
